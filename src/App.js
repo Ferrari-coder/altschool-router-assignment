63979f229a355860a7777e284d7a58a62e184b9f
Amudipe Emmanuel <abiemma84@gmail.com>
2022-10-25 12:24:39 +0100

    User api done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React, { Component } from "react";
+
 import NavBar from "./components/navbar";
 import ErrorBoundary from "./components/ErrorBoundary";
 import Error from "./components/error";
 
 class App extends Component {
- 
-  render(){
-  return (
-    <div className="App">
-      <ErrorBoundary>
-        <NavBar />
-      </ErrorBoundary>
-      <ErrorBoundary>
-
-      <Error/>
-      </ErrorBoundary>
-    </div>
-
-
-
-
-  )}
+  render() {
+    return (
+      <div className="App">
+        <ErrorBoundary>
+          <NavBar />
+        </ErrorBoundary>
+        <ErrorBoundary>
+          <Error />
+        </ErrorBoundary>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,45 +1,58 @@
 import React from "react";
 import Home from "./home";
 import User from "./user";
-import { BrowserRouter as Router, NavLink, Routes, Route } from "react-router-dom";
-import "./navbar.css"
+import {
+  BrowserRouter as Router,
+  NavLink,
+  Routes,
+  Route,
+} from "react-router-dom";
+import "./navbar.css";
 import PageNotFound from "./404";
 
 const NavBar = () => {
-
-    // if(true) {
-    //     throw new Error('error')
-    // }
-    return (
-        <Router>
-            <header>
-                <div className="nav">
-                    
-                    <ul className="navigation_items">
-                        <li><NavLink style={({ isActive }) =>
-                            isActive ? { color: "grey" } : { color: "purple" }
-                        } to="/" >Home </NavLink></li>
-                        <li><NavLink style={({ isActive }) =>
-                            isActive ? { color: "grey" } : { color: "purple" }
-                        } to="/user" >User</NavLink></li>
-                    </ul>
-                </div>
-            </header>
-            <main className="main">
-                <div className="container">
-                    <Routes>
-                        <Route path="/" element={<Home />}></Route>
-                        <Route path="/user" element={<User />}></Route>
-                        <Route path="*" element={<PageNotFound/>}></Route>
-                        
-                    </Routes>
-                    
-                </div>
-            </main>
-        </Router>
-
-
-    )
-}
+  // if(true) {
+  //     throw new Error('error')
+  // }
+  return (
+    <Router>
+      <header>
+        <div className="nav">
+          <ul className="navigation_items">
+            <li>
+              <NavLink
+                style={({ isActive }) =>
+                  isActive ? { color: "grey" } : { color: "purple" }
+                }
+                to="/"
+              >
+                Home{" "}
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                style={({ isActive }) =>
+                  isActive ? { color: "grey" } : { color: "purple" }
+                }
+                to="/users/page/1"
+              >
+                User
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </header>
+      <main className="main">
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/users/page/:page" element={<User />}></Route>
+            <Route path="*" element={<PageNotFound />}></Route>
+          </Routes>
+        </div>
+      </main>
+    </Router>
+  );
+};
 
 export default NavBar;
